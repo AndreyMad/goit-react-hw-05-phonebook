d79d2a4c40693cdf14f00a064161ecc80e4cfc19
AndreyMad <andrey.mad.87@gmail.com>
2020-02-27 21:58:38 +0200

    Updates

---
+++ precache-manifest.50003bfd432657d32490b45aea671f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6408448f5daafa68ef17c5359abba4a2",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "4a74a5b58a5591d3b6ff",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.59b7c085.chunk.css"
+  },
+  {
+    "revision": "eb84711b762bddf31729",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.47610bb3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.47610bb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a74a5b58a5591d3b6ff",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.5f04904e.chunk.js"
+  },
+  {
+    "revision": "141551164f89bdecb855",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.ba1e2898.js"
+  }
+]);
